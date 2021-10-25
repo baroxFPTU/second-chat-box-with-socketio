8d37a4a1c047e919427265a202af90f491ccb65b
Barox Phan <pqb.barox@gmail.com>
2021-10-25 15:04:57 +0700

    first commit

---
+++ public/main.js
@@ -0,0 +1,82 @@
+const socket = io("http://localhost:3000/");
+
+$(document).ready(function () {
+  $("#chatbox").hide();
+
+  $(".btn-start").click(function () {
+    const username = $("#username").val();
+    socket.emit("client-send-username", username);
+  });
+
+  $(".btn-logout").click(function () {
+    socket.emit("client-logout");
+    $("#chatbox").hide(1000);
+    $(".login-form").show(2000);
+    $("#username").val("");
+  });
+
+  $(".btn-send-message").click(function () {
+    socket.emit("client-send-message", $(".chatbox__input").val());
+    $(".chatbox__input").val("");
+  });
+
+  $(".chatbox__input").focus(function () {
+    console.log("Typing...");
+    socket.emit("client-typing-message");
+  });
+
+  $(".chatbox__input").blur(function () {
+    console.log("Blur...");
+    socket.emit("client-stop-typing-message");
+  });
+});
+
+// Socket events
+socket.on("server-send-failure-register", (failureMessage) => {
+  alert(failureMessage);
+});
+
+socket.on("server-send-success-register", (username) => {
+  alert("Successfully.");
+  $(".current-user").html(username);
+  $(".login-form").hide(1000);
+  $("#chatbox").show(1000);
+});
+
+socket.on("server-update-online-users", function (data) {
+  const listOnlineUsers = $(".list-users");
+
+  listOnlineUsers.html("");
+  $.each(data, function (index, username) {
+    listOnlineUsers.append(
+      `<li class="user" data-index="${index}">${username}</li>`
+    );
+  });
+});
+
+socket.on("server-send-message", function ({ username, message }) {
+  $(".chatbox__messages").append(
+    `<div class="chatbox__message"><span class="chatbox__message-username">${username}</span> ${message}</div>`
+  );
+});
+
+socket.on("server-update-typing-state", function (isTyping) {
+  if (!isTyping) {
+    return $(".chatbox__typing").hide(1000);
+  }
+
+  return $(".chatbox__messages").append(
+    `<div class="chatbox__typing">Someone is typing...</div>`
+  );
+});
+// TODO
+// CLIENT - emit send new username to server
+// SERVER - catch the failure case
+// SERVER - catch the success case
+// SERVER - emit list of online user
+
+//SOCKET map
+// client-send-username
+// server-send-failure-register
+// server-send-success-register
+// server-update-online-users
---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const { on } = require("stream");
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+const port = process.env.PORT || 3000;
+server.listen(port);
+
+const onlineUsers = [];
+
+// Socketio
+io.on("connect", function (socket) {
+  console.log("Connected - ", socket.id);
+
+  socket.on("client-send-username", function (name) {
+    const isExit = onlineUsers.find((username) => username === name) ?? false;
+
+    if (isExit) {
+      socket.emit(
+        "server-send-failure-register",
+        "Username has been registered."
+      );
+    } else {
+      onlineUsers.push(name);
+      socket.username = name;
+      socket.emit("server-send-success-register", name);
+      io.sockets.emit("server-update-online-users", onlineUsers);
+    }
+  });
+
+  socket.on("client-send-message", function (message) {
+    io.sockets.emit("server-send-message", {
+      username: socket.username,
+      message: message,
+    });
+  });
+
+  socket.on("client-typing-message", function () {
+    socket.broadcast.emit("server-update-typing-state", true);
+  });
+
+  socket.on("client-stop-typing-message", function () {
+    socket.broadcast.emit("server-update-typing-state", false);
+  });
+
+  socket.on("client-logout", function () {
+    onlineUsers.splice(onlineUsers.indexOf(socket.username), 1);
+    console.log(onlineUsers);
+    socket.username = null;
+    socket.broadcast.emit("server-update-online-users", onlineUsers);
+  });
+});
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
